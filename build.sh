1c0ba1237edd87136b5b7f0900b1faee94639337
high on tantor <josephvenable@verizon.net>
2026-06-21 19:48:40 -0400

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+cd "${0%/*}/../"
+# Some platforms do not accept the -o flag for uname; any warnings printed to the terminal concerning this can be safely ignored...
+if [ `uname -o` = "Msys" ]; then
+  g++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static -lntdll -lshell32 -lole32 -luuid -Wl,--subsystem,console; ./xproc.exe;
+elif [ `uname` = "Darwin" ]; then
+  clang++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -mmacos-version-min=10.13 -arch arm64 -arch x86_64; ./xproc;
+elif [ `uname -o` = "GNU/Linux" ]; then
+  if [ -f "/bin/g++" ]; then
+    g++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lpthread -static; ./xproc;
+  else
+    clang++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -lpthread; ./xproc;
+  fi;
+elif [ `uname` = "FreeBSD" ]; then
+  clang++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -lelf -lkvm -lpthread -static; ./xproc;
+elif [ `uname` = "DragonFly" ]; then
+  g++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
+elif [ `uname` = "NetBSD" ]; then
+  g++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
+elif [ `uname` = "OpenBSD" ]; then
+  clang++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lpthread -static; ./xproc;
+elif [ `uname` = "SunOS" ]; then
+  if [ `uname -o` = "illumos" ]; then
+    g++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -static-libgcc -lkvm -lproc -lpthread; ./xproc;
+  else
+    g++ main.cpp apifilesystem/filesystem.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -lkvm -lproc -lpthread; ./xproc;
+  fi;
+fi;
---
+++ main.cpp
@@ -0,0 +1,83 @@
+/*
+
+ MIT License
+
+ Copyright © 2021-2026 Samuel Venable
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+*/
+
+#include <apiprocess/process.hpp>
+#include <apifilesystem/filesystem.hpp>
+#if (defined(__apiprocess_supported__) && defined(__apifilesystem_supported__))
+#include <iostream>
+
+#include <cctype>
+#include <cstdio>
+#include <cstddef>
+#include <cstring>
+
+#if ((defined(_WIN32) || defined(_WIN64)) && defined(_MSC_VER))
+#pragma comment(linker, "/subsystem:console /ENTRY:mainCRTStartup")
+#endif
+#endif
+
+int main(int argc, char **argv) {
+  #if (defined(__apiprocess_supported__) && defined(__apifilesystem_supported__))
+  std::string scratch_game;
+  if (argc == 2 && argv[1] && strcmp(argv[1], "")) {
+    scratch_game = argv[1];
+  } else {
+    ngs::fs::environment_unset_variable("IMGUI_DIALOG_PARENT");
+    ngs::fs::environment_set_variable("IMGUI_DIALOG_WIDTH", std::to_string(720));
+    ngs::fs::environment_set_variable("IMGUI_DIALOG_HEIGHT", std::to_string(394));
+    ngs::fs::environment_set_variable("IMGUI_DIALOG_THEME", std::to_string(2));
+    ngs::fs::environment_set_variable("IMGUI_TEXT_COLOR_0", std::to_string(1));
+    ngs::fs::environment_set_variable("IMGUI_TEXT_COLOR_1", std::to_string(1));
+    ngs::fs::environment_set_variable("IMGUI_TEXT_COLOR_2", std::to_string(1));
+    ngs::fs::environment_set_variable("IMGUI_HEAD_COLOR_0", std::to_string(0.55));
+    ngs::fs::environment_set_variable("IMGUI_HEAD_COLOR_1", std::to_string(0.35));
+    ngs::fs::environment_set_variable("IMGUI_HEAD_COLOR_2", std::to_string(0.55));
+    ngs::fs::environment_set_variable("IMGUI_AREA_COLOR_0", std::to_string(0.18));
+    ngs::fs::environment_set_variable("IMGUI_AREA_COLOR_1", std::to_string(0.18));
+    ngs::fs::environment_set_variable("IMGUI_AREA_COLOR_2", std::to_string(0.18));
+    ngs::fs::environment_set_variable("IMGUI_BODY_COLOR_0", std::to_string(1));
+    ngs::fs::environment_set_variable("IMGUI_BODY_COLOR_1", std::to_string(1));
+    ngs::fs::environment_set_variable("IMGUI_BODY_COLOR_2", std::to_string(1));
+    ngs::fs::environment_set_variable("IMGUI_POPS_COLOR_0", std::to_string(0.07));
+    ngs::fs::environment_set_variable("IMGUI_POPS_COLOR_1", std::to_string(0.07));
+    ngs::fs::environment_set_variable("IMGUI_POPS_COLOR_2", std::to_string(0.07));
+    ngs::fs::environment_set_variable("IMGUI_FONT_SIZE", std::to_string(20));
+    ngs::ps::ngs_proc_id_t proc_id = ngs::ps::spawn_child_proc_id(ngs::fs::executable_get_directory() + "CLI/ImFileDialog/filedialogs/filedialogs --get-open-filename-ext \"Scratch Game Files (*.sb3)|*.sb3\" \"\" \"\" \"Select a Scratch *.sb3 Game File to play...\"", true);
+    scratch_game = ngs::ps::read_from_stdout_for_child_proc_id(proc_id);
+    while (!scratch_game.empty() && (scratch_game.back() == '\n' || scratch_game.back() == '\r')) {
+      scratch_game.pop_back();
+    }
+    ngs::ps::free_stdout_for_child_proc_id(proc_id);
+    ngs::ps::free_stdin_for_child_proc_id(proc_id);
+  }
+  if (!scratch_game.empty()) {
+    ngs::ps::ngs_proc_id_t proc_id = ngs::ps::spawn_child_proc_id(ngs::fs::executable_get_directory() + "CLI/ScratchEverywhere/scratch-everywhere-pc \"" + scratch_game + "\"", true);
+    ngs::ps::free_stdout_for_child_proc_id(proc_id);
+    ngs::ps::free_stdin_for_child_proc_id(proc_id);
+  }
+  #endif
+  return 0;
+}
